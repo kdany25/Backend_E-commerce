3d2d7b20e475a8189ca6c8362db42ff7227e871b
kdany25 <kabadany25@gmail.com>
2022-01-28 07:11:56 +0200

    db

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const  Mongoose  = require('mongoose');
+const dotenv=require("dotenv")
 
 const app = express();
+dotenv.config()
+
+Mongoose.connect(process.env.MONGO_URL)
+.then(()=> console.log("DBconnection successfull")).catch((err)=>{console.log(err)});
 app.listen(5000,()=>{
     console.log('Backend server is running')
 })
