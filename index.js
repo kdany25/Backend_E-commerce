b342b1608bd01ebb9437efdfcf9234ea9bbafc52
kdany25 <kabadany25@gmail.com>
2022-01-28 09:02:17 +0200

    --

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const  Mongoose  = require('mongoose');
-const dotenv=require("dotenv")
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+import express from "express";
+import Mongoose from "mongoose";
+import useRouter from "./routes/userRouter"
 
 const app = express();
-dotenv.config()
 
 Mongoose.connect(process.env.MONGO_URL)
-.then(()=> console.log("DBconnection successfull")).catch((err)=>{console.log(err)});
-app.listen(5000,()=>{
-    console.log('Backend server is running')
-})
+  .then(() => console.log("DBconnection successfull"))
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(express.json())
+  app.use("/api/user", useRouter)
+app.listen(5000, () => {
+  console.log("Backend server is running");
+});
