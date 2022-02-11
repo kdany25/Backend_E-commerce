d7b79604a8bcc40fabfedf44964a7ed485db0817
kdany25 <kabadany25@gmail.com>
2022-02-11 15:20:28 +0200

    rf:(payment)

--- routes/user.js
+++ routes/user.js
@@ -63,7 +63,7 @@ router.get("/",verifyTokenAndAdmin,  async (req, res) => {
   }
 });
 
-router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+router.get("/stats",  async (req, res) => {
   const date = new Date();
   const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
 
