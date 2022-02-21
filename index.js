955adf578cc5e9526f1c3e2624e667883c8ce0cb
kdany25 <kabadany25@gmail.com>
2022-02-21 21:46:20 +0200

    bg-fix(cors)

--- index.js
+++ index.js
@@ -19,11 +19,12 @@ Mongoose.connect(process.env.MONGO_URL)
   .catch((err) => {
     console.log(err);
   });
-app.use(
-  cors({
-    origin: "*",  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-  })
-);
+  const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", cors(), userAuth);
 app.use("/api/users",cors(), userRoute);
