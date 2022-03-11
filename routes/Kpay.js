29741eb2a651ee15a060ebe10075c70d07efd891
kdany25 <kabadany25@gmail.com>
2022-03-11 16:31:39 +0200

    bgfix

--- routes/Kpay.js
+++ routes/Kpay.js
@@ -1,35 +1,31 @@
 import express from "express";
-import { v4 as uuidv4 } from 'uuid';
-import fetch from 'cross-fetch';
+import { v4 as uuidv4 } from "uuid";
+import fetch from "cross-fetch";
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+  const payment = {
+    msisdn: "0788730199",
+    details: "order",
+    refid: uuidv4(),
+    amount: 4200,
+    email: "kabadany25@gmail.com",
+    cname: "kaneza",
+    cnumber: "0788730199",
+    pmethod: "momo",
+    retailerid: "23",
+    returl: "myclikkreact.herokuapp.com/",
+    redirecturl: "myclikkreact.herokuapp.com/",
+    bankid: "130",
+  };
 
-
-router.post("/",async(req,res)=>{
-    
-    const payment = {
-        "msisdn" : "0788730199" ,
-        "details" : "order" ,
-        "refid" : uuidv4(),
-        "amount" : 4200 ,
-        "email" :"kabadany25@gmail.com" ,
-        "cname" : "kaneza" ,
-        "cnumber" : "0788730199" ,
-        "pmethod" : "momo" ,
-        "retailerid" : "23",
-        "returl" : "myclikkreact.herokuapp.com/",
-        "redirecturl" : "myclikkreact.herokuapp.com/",
-        "bankid" :"130"
-    }
-
-    const response = await  fetch('https://pay.esicia.com', {
-	method: 'POST',
-	body: JSON.stringify(payment),
-	headers: {
-        'Authorization': 'Basic ' + ('myclikk:2Xqz2d')
-    }
+  const response = await fetch("https://pay.esicia.com", {
+    method: "POST",
+    body: JSON.stringify(payment),
+    headers: {
+      Authorization: "Basic " + "myclikk:2Xqz2d",
+    },
+  });
 });
-
-})
 
 export default router;
